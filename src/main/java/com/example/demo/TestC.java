da45f5a4647a906210f3f042de5a495f93192d01
thanh-desktop <quocthanh4738@gmail.com>
2025-12-10 22:42:34 +0700

    AAAAAAA

--- src/main/java/com/example/demo/TestC.java
+++ src/main/java/com/example/demo/TestC.java
@@ -9,6 +9,14 @@ public class TestC {
     private int g = 1;
     private int h = 1;
     private int j = 1;
+    private int ac = 1;
+    private int ba = 1;
+    private int Cf = 1;
+    private int dt = 1;
+    private int ec = 1;
+    private int ga = 1;
+    private int hz = 1;
+    private int jq = 1;
 
     int getInt(){
         return 1;
