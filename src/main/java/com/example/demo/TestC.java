0c9c6993b6100404528353d9e0f369a3ee13ed7f
thanh-desktop <quocthanh4738@gmail.com>
2025-12-10 22:40:00 +0700

    EEEE

--- src/main/java/com/example/demo/TestC.java
+++ src/main/java/com/example/demo/TestC.java
@@ -1,15 +1,6 @@
 package com.example.demo;
 
 public class TestC {
-    private int a = 1;
-    private int b = 1;
-    private int C = 1;
-    private int d = 1;
-    private int e = 1;
-    private int g = 1;
-    private int h = 1;
-    private int j = 1;
-    private int ad = 1;
     private String l = "zero";
     private String qf = "zero";
     private String ec = "zero";
@@ -17,22 +8,8 @@ public class TestC {
     private String lg = "zero";
     private String lh = "zero";
     private String lj = "zero";
-    private int bc = 1;
-    private int Cz = 1;
-    private int dcw = 1;
-    private int efa = 1;
-    private int gg = 1;
-    private int hq = 1;
-    private int jp = 1;
 
     String getString(){
         return "avư";
-    }
-    int getInt(){
-        return 1;
-    }
-
-    int getZInt(){
-        return 5;
     }
 }
