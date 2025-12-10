74212d82cfe5e00bf7e2633f055e0ab50b108e4b
thanh-desktop <quocthanh4738@gmail.com>
2025-12-10 21:51:29 +0700

    a

---
+++ src/main/java/com/example/demo/TestC.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class TestC {
+    private int a = 1;
+    private int b = 1;
+    private int C = 1;
+    private int d = 1;
+    private int e = 1;
+    private int g = 1;
+    private int h = 1;
+    private int j = 1;
+
+    int getInt(){
+        return 1;
+    }
+}
